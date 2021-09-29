d6f129cd85764f84a17c8a14ced2629e730c21fa
Omkar Pilankar <omkarpilankar5@gmail.com>
2021-09-30 00:12:35 +0530

    Made some minor changes

--- src/main/kotlin/com/documentsorganizer/app/MyApp.kt
+++ src/main/kotlin/com/documentsorganizer/app/MyApp.kt
@@ -1,6 +1,7 @@
 package com.documentsorganizer.app
 
 import com.documentsorganizer.view.MainView
+import javafx.scene.image.Image
 import javafx.stage.Stage
 import tornadofx.*
 
@@ -11,6 +12,7 @@ class MyApp: App(MainView::class) {
             height = 750.0
             width = 1000.0
             isResizable = false
+            addStageIcon(Image("logo.png"))
         }
         super.start(stage)
     }
