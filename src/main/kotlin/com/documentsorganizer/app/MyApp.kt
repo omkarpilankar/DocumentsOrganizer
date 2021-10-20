ba9931c8c73b9ceb291d25c08c2a9296b4bfec4c
Omkar Pilankar <omkarpilankar5@gmail.com>
2021-10-20 09:48:20 +0530

    Update MyApp.kt

--- src/main/kotlin/com/documentsorganizer/app/MyApp.kt
+++ src/main/kotlin/com/documentsorganizer/app/MyApp.kt
@@ -5,10 +5,12 @@ import javafx.scene.image.Image
 import javafx.stage.Stage
 import tornadofx.*
 
+// Entry point of the application. Requires MainView.kt file.
 class MyApp : App(MainView::class) {
 
     override fun start(stage: Stage) {
         with(stage) {
+            //set height and width
             height = 775.0
             width = 1005.0
             isResizable = false
